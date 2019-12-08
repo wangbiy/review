61283e3ec3c0697ef369638081db95736c992806
wangbiy <2487036948@qq.com>
2019-12-08 21:13:46 +0800

    回调函数复习、函数指针数组实现转移表

--- test_2019_12_7/qsort.c
+++ test_2019_12_7/qsort.c
@@ -1,18 +1,58 @@
 #include <stdio.h>
+#include <string.h>
 //实现整形数组元素的排序
-int int_cmp(const void* p1,const void* p2)
+//int int_cmp(const void* p1,const void* p2)
+//{
+    //return (*(int*)p1-*(int*)p2);
+//}
+struct S
 {
-    return (*(int*)p1-*(int*)p2);
+	char name[20];
+	int age;
+};
+//实现比较函数
+void qsort(void* base,size_t num,size_t width,int (*cmp)(const void* e1,const void* e2));
+int cmp_S_by_name(const void* e1,const void* e2)
+{
+	return strcmp(((struct S*)e1)->name,((struct S*)e2)->name);
+}
+
+int cmp_S_by_age(const void* e1,const void* e2)
+{
+	return (((struct S*)e1)->age-((struct S*)e2)->age);
+}
+void test1()
+{
+	struct S arr[3]={{"x",20},{"l",10},{"w",15}};
+	qsort(arr,3,sizeof(struct S),cmp_S_by_name);
+	for(int i=0;i<3;++i)
+	{
+		printf("%s ",arr[i].name);
+
+	}
+	printf("\n");
+}
+void test2()
+{
+	struct S arr[3]={{"x",20},{"l",10},{"w",15}};
+	qsort(arr,3,sizeof(struct S),cmp_S_by_age);
+	for(int i=0;i<3;++i)
+	{
+		printf("%d ",arr[i].age);
+	}
+	printf("\n");
 }
 int main()
 {
-    int arr[]={1,3,4,6,2,7,9,0};
-	int i=0;
-	qsort(arr,sizeof(arr)/sizeof(arr[0]),sizeof(int),int_cmp);
-	for(i=0;i<sizeof(arr)/sizeof(arr[0]);++i)
-	{
-		printf("%d",arr[i]);
-	}
-	printf("\n");
+	test1();
+	test2();
+   // int arr[]={1,3,4,6,2,7,9,0};
+   // int i=0;
+   // qsort(arr,sizeof(arr)/sizeof(arr[0]),sizeof(int),int_cmp);
+   // for(i=0;i<sizeof(arr)/sizeof(arr[0]);++i)
+   // {
+   // 	printf("%d",arr[i]);
+   // }
+   // printf("\n");
 	return 0;
 }
