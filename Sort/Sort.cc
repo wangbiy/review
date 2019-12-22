65b5995c68d8f9da069d4391ac9c6c3865b545d7
wangbiy <2487036948@qq.com>
2019-12-22 21:16:40 +0800

    排序复习

--- Sort/Sort.cc
+++ Sort/Sort.cc
@@ -49,12 +49,124 @@ void ShellSort(int* arr,int size)
 			break;
 	}
 }
+//简单选择排序
+void SelectSort1(int* arr,int size)
+{
+	for(int i=0;i<size;++i)//比较趟数
+	{
+		int max=0;//假设无序区间第一个数最大
+		for(int j=1;j<size-i;++j)//无序区间
+		{
+			if(arr[j]>arr[max])
+			{
+				max=j;
+			}
+		}
+		//最大的与无序区间最后一个交换
+		int t=arr[max];
+		arr[max]=arr[size-i-1];
+		arr[size-i-1]=t;
+	}
+}
+void SelectSort2(int* arr,int size)
+{
+	for(int i=0;i<size;++i)//比较的趟数
+	{
+		int min=i;//假设最小的是无序区间的第一个
+		for(int j=1;j<size-i;++j)//无序区间
+		{
+			if(arr[j]<arr[min])
+			{
+				min=i;
+			}
+		}
+		int t=arr[min];
+		arr[min]=arr[i];
+		arr[i]=t;
+	}
+}
+void swap(int& a,int& b)
+{
+	int t=a;
+	a=b;
+	b=t;
+}
+void SelectSort3(int* arr,int size)
+{
+	int left=0;
+	int right=size-1;
+	while(left<right)
+	{
+		int min=left;
+		int max=left;
+		for(int i=left+1;i<=right;++i)
+		{
+			if(arr[i]<arr[min])
+			{
+				min=i;
+			}
+			if(arr[i]>arr[max])
+			{
+				max=i;
+			}
+		}
+		swap(arr[min],arr[left]);
+		if(max==left)
+		{
+			max=min;
+		}
+		swap(arr[max],arr[right]);
+		left++;
+		right--;
+	}
+}
+//堆排序
+void heapify(int* arr,int size,int index)
+{
+	int left=2*index+1;
+	int right=2*index+2;
+	if(left>=size)
+		return;
+	int max=left;
+	if(right<size && arr[right]>arr[left])
+	{
+		max=right;
+	}
+	if(arr[index]>arr[max])
+		return;
+	swap(arr[index],arr[max]);
+	heapify(arr,size,max);
+}
+void CreateHeap(int* arr,int size)
+{
+	for(int i=(size-1-1)/2;i>=0;--i)
+	{
+		heapify(arr,size,i);//建大堆
+	}
+}
+void HeapSort(int* arr,int size)
+{
+	CreateHeap(arr,size);
+	for(int i=0;i<size;++i)
+	{
+		swap(arr[0],arr[size-i-1]);
+		heapify(arr,size-i-1,0);
+	}
+}
 int main()
 {
 	int arr[]={1,8,7,9,10,0,24,76,2};
 	InsertSort(arr,sizeof(arr)/sizeof(arr[0]));
 	Print(arr,sizeof(arr)/sizeof(arr[0]));
 	ShellSort(arr,sizeof(arr)/sizeof(arr[0]));
+	Print(arr,sizeof(arr)/sizeof(arr[0]));
+	SelectSort1(arr,sizeof(arr)/sizeof(arr[0]));
+	Print(arr,sizeof(arr)/sizeof(arr[0]));
+	SelectSort2(arr,sizeof(arr)/sizeof(arr[0]));
+	Print(arr,sizeof(arr)/sizeof(arr[0]));
+	SelectSort3(arr,sizeof(arr)/sizeof(arr[0]));
+	Print(arr,sizeof(arr)/sizeof(arr[0]));
+	HeapSort(arr,sizeof(arr)/sizeof(arr[0]));
 	Print(arr,sizeof(arr)/sizeof(arr[0]));
 	return 0;
 }
