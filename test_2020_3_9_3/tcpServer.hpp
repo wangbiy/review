60fcd332968fe0a566840cfdacfe5e234369af5e
wangbiy <2487036948@qq.com>
2020-03-09 20:05:23 +0800

    Linux基于单进程或者多进程的tcp网络通信复习

--- test_2020_3_9_3/tcpServer.hpp
+++ test_2020_3_9_3/tcpServer.hpp
@@ -9,6 +9,7 @@ using namespace std;
 #include <cstring>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 class Server
 {
 	private:
@@ -67,6 +68,7 @@ class Server
 					cerr<<"read error"<<endl;
 			}
 		}
+#if 0
 		//开始接受连接，进行收发数据
 		void Start()
 		{
@@ -84,6 +86,35 @@ class Server
 				//进行读写数据
 				ServiceIO(sock);//对新连接进行IO操作
 				close(sock);//关掉新连接，以防内存泄漏
+			}
+		}
+#endif
+		//上述是使用单进程实现的服务器，如果是多连接的，就不能和服务器进行正确的通信，是因为我们accept之后，就一直循环尝试read，没有继续调用accept，导致不能接受新的请求，因此，我们要实现一个多进程的基于TCP的网络程序，只需要将accept这里改一下
+		void Start()
+		{
+			//使用fork创建子进程，在接受连接的过程中，创建子进程，子进程可以看到父进程的文件描述符，也继承了父进程的接受连接的操作，子进程进行IO操作，父进程不能以阻塞方式等待，不然就还是单进程的操作，可以通过SIGCHLD信号来通知父进程忽略方式等待，父子进程各自有一个文件描述符表，子进程最好关掉文件描述符，以防内存泄漏，最后父进程关掉文件描述符
+			struct sockaddr_in peer;//对端
+			signal(SIGCHLD,SIG_IGN);//将父进程设置为忽略
+			while(true)
+			{
+				socklen_t len=sizeof(peer);
+				int sock=accept(listen_sock,(struct sockaddr*)&peer,&len);
+				if(sock<0)
+				{
+					cerr<<"accept error"<<endl;
+					continue;
+				}
+				cout<<"get a new link[IP:PORT]"<<inet_ntoa(peer.sin_addr)<<":"<<ntohs(peer.sin_port)<<endl;//拿到客户端的ip地址，以点分二进制的方式打印，用地址转换函数将4字节的字符串转换为点分二进制的ip地址，使用inet_ntoa接口，而打印port要将网络号转换为主机号
+				pid_t id=fork();
+				if(id==0)//child
+				{
+					close(listen_sock);//不需要这个监听套接字
+					ServiceIO(sock);
+					close(sock);//使用完关闭
+					exit(0);//子进程退出
+				}
+				//father
+				close(sock);//父进程接收子进程SIGCHLD信号，关闭即可
 			}
 		}
 };
